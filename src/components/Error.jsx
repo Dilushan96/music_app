f39a0463bcfebcc0991479a5b01cb4d4330609d9
Dilushan96 <dilushan.rajapakshe@gmail.com>
2022-11-05 22:36:04 +0530

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+
+const Error = () => (
+  <div className="w-full flex justify-center items-center">Error
+ <h1 className="font-bold text-2xl text-white mt-2">{title || "Something went wrong.."}</h1>
+  </div>
+  
+);
+
+export default Error;
---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,17 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+export const shazamCoreAPi =createApi({
+    reducerPath:'shazamCoreAPi',
+    baseQuery:fetchBaseQuery({
+            baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+            prepareHeaders:(headers)=> {
+                headers.set('X-RapidAPI-Key','b44d396f7dmsha3d08e2eb621984p16b2adjsn87b60233702e');
+                return headers;
+            }
+          }),
+          endpoints:(builder) =>({
+            getTopCharts:builder.query({query:()=> '/charts/world'}),
+          }),
+});
+export const {
+    useGetTopChartsQuery,
+}=shazamCoreAPi;
